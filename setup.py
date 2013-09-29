f6668fe1f3401138474cd8b2d6fb7fc52ffc0374
Asko Soukka <asko.soukka@iki.fi>
2013-09-29 21:57:40 +0300

    Back to development: 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinxcontrib-robotframework',
-    version='0.4.0',
+    version='0.4.1.dev0',
     description='Robot Framework plugin for Sphinx',
     long_description=(open("README.rst").read() + "\n" +
                       open("CHANGES.txt").read()),
